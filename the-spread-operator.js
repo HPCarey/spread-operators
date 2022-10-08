4049066936e29230a2aafecea27db49884e0c731
HPCarey <hannahpcarey@gmail.com>
2022-10-08 15:57:07 +0000

    add example from challenge

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -29,3 +29,31 @@ console.log("Third object:", obj3);
 // Copying only part of an array/object
 let arr5 = [...arr1, {...obj1}, ...arr3, "x", "y", "z"];
 console.log(arr5);
+
+// Copying nested data structure, ie. copy an array inside an object inside an arrary
+// Copy the array subjects from the first object inside the array students. Assign it the variable subjects
+let students = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 80, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 63, maths: 87, art: 95},
+    }
+];
+//solution
+let subjects = [...students[0].subjects];
+let subjects2 = [...students[1].subjects];
+console.log(subjects);
+console.log(subjects2);
